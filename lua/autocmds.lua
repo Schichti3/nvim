486bb5007c67efbb0ede95bc1929ee6fa438fbb7
Schichti <orasdemo123@gmail.com>
2025-11-30 16:16:52 +0100

    removed auto comment on next line when new lining on a comment

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -4,3 +4,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 		vim.highlight.on_yank()
 	end,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+	callback = function()
+		vim.opt_local.formatoptions:remove({'c', 'r', 'o'})
+	end
+})
